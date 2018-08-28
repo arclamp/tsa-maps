15cdfb3be02dfcce63adb38ea8083434b576c5c4
Roni Choudhury <roni.choudhury@kitware.com>
2018-08-28 17:28:26 -0400

    Add enough stuff for a working build

---
+++ webpack.config.babel.js
@@ -0,0 +1,32 @@
+import path from 'path';
+import HtmlPlugin from 'html-webpack-plugin';
+
+export default {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index.js'
+  },
+  plugins: [
+    new HtmlPlugin({
+      title: 'TSA Maps',
+      filename: 'index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/env'
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
