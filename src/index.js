a143e4750d14226125d1a38673e56b9599448b0e
Roni Choudhury <roni.choudhury@kitware.com>
2018-08-28 18:58:54 -0400

    Draw a dot on Chicago

--- src/index.js
+++ src/index.js
@@ -8,3 +8,24 @@ const map = geojs.map({
   node: '#map'
 });
 map.createLayer('osm');
+
+const layer = map.createLayer('feature', {
+  features: ['point']
+});
+
+const points = layer.createFeature('point', {
+  primitiveShape: 'sprite'
+});
+
+points.data([{
+  y: 41.8781,
+  x: -87.6298
+}]);
+
+points.style({
+  radius: (d, i) => {
+    console.log(d, i);
+    return 50;
+  }
+});
+points.draw();
