fc2682b1f3d0313a071d3216b689cc7a96c93f5a
Roni Choudhury <roni.choudhury@kitware.com>
2018-08-29 15:43:31 -0400

    Import lat/long data into raw data output

--- data/process.py
+++ data/process.py
@@ -3,6 +3,18 @@ import json
 import sys
 
 
+def process(data, airports):
+    for row in data:
+        row['Airline Name'] = row['Airline Name'].strip()
+
+        code = row['Airport Code']
+        if code in airports:
+            row['lat'] = airports[row['Airport Code']]['lat']
+            row['lon'] = airports[row['Airport Code']]['lon']
+
+    return filter(lambda x: x['Airport Code'] in airports, data)
+
+
 def csv2dict(filename):
     with open(filename) as f:
         reader = csv.reader(f)
@@ -19,7 +31,10 @@ def concatenate(lists):
 
 
 def main():
-    data = concatenate(map(csv2dict, sys.argv[1:]))
+    with open(sys.argv[1]) as f:
+        airport_data = json.loads(f.read())
+
+    data = process(concatenate(map(csv2dict, sys.argv[2:])), airport_data)
 
     print json.dumps(data, indent=2)
 
