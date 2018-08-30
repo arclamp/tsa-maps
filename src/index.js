51e510357972e7ef0e657774890731253863d03f
Roni Choudhury <roni.choudhury@kitware.com>
2018-08-30 16:52:51 -0400

    Display a random chart

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import { select } from 'd3-selection';
 import geo from 'geojs';
+import c3 from 'c3';
+import 'c3/c3.css';
 
 import content from './index.pug';
 import data from './data.json';
@@ -54,6 +57,51 @@ points.data(pointData);
 
 points.geoOn(geo.event.feature.mouseclick, evt => {
   console.log(evt);
+
+  makeChart();
 });
 
 points.draw();
+
+const ui = map.createLayer('ui');
+const legend = ui.createWidget('colorLegend', {
+  position: {
+    right: 20,
+    top: 10
+  },
+  categories: [{
+    name: 'Population',
+    type: 'discrete',
+    scale: 'linear',
+    domain: [500e3, 6e5],
+    colors: ['rgb(237,248,233)', 'rgb(186,228,179)', 'rgb(116,196,118)', 'rgb(35,139,69)']
+  }]
+});
+
+const chartWidget = ui.createWidget('svg');
+select(chartWidget.canvas())
+  .style('width', '400px')
+  .style('height', '400px');
+
+function makeChart ()  {
+  select(chartWidget.canvas()).selectAll('*').remove();
+
+  c3.generate({
+    bindto: chartWidget.canvas(),
+    data: {
+      x: 'x',
+      columns: [
+        ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
+        ['data1', 10 * Math.random(), 10 * Math.random(), 10 * Math.random(), 10 * Math.random(), 10 * Math.random(), 10 * Math.random()]
+      ]
+    },
+    axis: {
+      x: {
+        type: 'timeseries',
+        tick: {
+          format: '%Y-%m'
+        }
+      }
+    }
+  });
+}
